d0aee08627904bff02e60965b325fcd8e4331cbe
Guilherme <gui.duarte.2012@gmail.com>
2022-05-09 17:37:14 -0300

    .

---
+++ exercicio_velocidade_maxima.py
@@ -0,0 +1,10 @@
+velocidade_maxima = 80
+velocidade_atual = int(input("Digite a velocidade em Km/h: "))
+if velocidade_atual <= velocidade_maxima:
+ print('Não houve multa!')
+elif velocidade_atual > velocidade_maxima and velocidade_atual <= velocidade_maxima + 10:
+ print('Você levou multa leve.')
+elif velocidade_atual >= velocidade_maxima + 11 and velocidade_atual <= velocidade_maxima + 20:
+ print('Você levou multa grave!')
+elif velocidade_atual >= velocidade_maxima + 20:
+ print('Você levou multa gravísssima!!!')
